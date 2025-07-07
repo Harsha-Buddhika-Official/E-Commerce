d0d115d8ed8d17bb8f9946b59b621ebee5bcd8cc
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-07-07 15:44:39 +0530

    Update SignUp.jsx

--- Client/src/SignUp.jsx
+++ Client/src/SignUp.jsx
@@ -1,16 +1,6 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { 
-  User, 
-  Mail, 
-  Lock, 
-  Eye, 
-  EyeOff, 
-  Phone, 
-  ShoppingBag, 
-  Sparkles,
-  AlertCircle
-} from 'lucide-react';
+import { User, Mail, Lock, Eye, EyeOff, Phone, ShoppingBag, AlertCircle} from 'lucide-react';
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
