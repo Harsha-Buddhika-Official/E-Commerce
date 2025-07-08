f7215442ba32f1d1020ae99ca6f85cf74a417ab5
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-07-08 14:05:19 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import SignUp from './SignUp'
 import Login from './Login'
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      {/* <Login/> */}
-      <SignUp/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
